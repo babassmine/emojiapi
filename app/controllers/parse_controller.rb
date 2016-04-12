f3f6ed895927a38cf9ebaff2073971dde775289d
Abass Sesay <babassmine@Abasss-MacBook-Pro.local>
2016-04-11 22:27:13 -0400

    add logic for parsing sentences and replacing words with emojis

--- app/controllers/parse_controller.rb
+++ app/controllers/parse_controller.rb
@@ -1,6 +1,34 @@
 class ParseController < ApplicationController
   def word
-    sent = params[:s]
-    render :json => {converted: sent}.to_json
+    sentence = params[:s].split(' ')
+    response = []
+    sentence.each do |word|
+      sing_word = word.singularize
+      tag = Tag.where(name: sing_word).first
+      if tag
+        emojis = tag.emojis
+        index = Random.rand(emojis.count)
+        if is_plural?(word)
+          response << emojis[index].char_dec * 2
+        else
+          response << emojis[index].char_dec
+        end
+      else
+        response << word
+      end
+    end
+    render :json => {
+      converted: response.join(' '),
+      original: sentence
+     }.to_json
+  end
+
+  private
+
+  def is_plural? word
+    if word == word.pluralize
+      return true
+    end
+    false
   end
 end
